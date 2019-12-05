0aefdf8ba014fa9ad69aa43caed4d73f0f3de23b
Amanda <amanda@gaap.co.za>
2019-12-05 15:01:08 +0200

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿Alter PROCEDURE EmployeesToUpdate
+    @EmployeeID INT,
+    @YearsInExperience INT
+AS
+BEGIN
+UPDATE dbo.Employees
+    SET YearsInExperience = @YearsInExperience
+        WHERE EmployeeID = @EmployeeID
+END
---
+++ StaffList/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffList
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int YearInExperience { get; set; }
+       
+        
+    }
+
+    public interface IClass
+    {
+        void Add(Class person);
+        Class Find(string firstName, string lastName, int yearInExperience);
+    }
+
+ 
+
+}
